76b0cc716de0a6c2b03ee7b2254e2f867967eeb3
Marquavious Draggon <marq.draggon@gmail.com>
2017-02-17 10:42:18 -0800

    init

---
+++ gist_coding_challenge/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  gist_coding_challenge
+//
+//  Created by Marquavious on 2/17/17.
+//  Copyright © 2017 Marquavious Draggon. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ gist_coding_challenge/MyGistsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyGistsTableViewCell.swift
+//  gist_coding_challenge
+//
+//  Created by Marquavious on 2/17/17.
+//  Copyright © 2017 Marquavious Draggon. All rights reserved.
+//
+
+import UIKit
+
+class MyGistsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
